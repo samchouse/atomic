bb9d6dc47add1f4e57d7b5e4787c6c88d48c8853
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-02-23 16:40:06 -0500

    fix: build broke

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,14 +1,16 @@
-module.exports = {
-  displayName: 'gateway',
+/* eslint-disable */
+export default {
+  displayName: 'backend',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json'
     }
   },
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/gateway'
+  coverageDirectory: '../../coverage/apps/backend'
 };
